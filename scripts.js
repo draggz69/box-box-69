46d1b2e26f5216f0730c77c9e0e50c3d7e834865
draggz69 <dragtron3104@gmail.com>
2024-08-10 00:31:59 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,200 +1,170 @@
-const red = document.querySelector(".red");
-const blue = document.querySelector(".blue");
-const yellow = document.querySelector(".yellow");
-const green = document.querySelector(".green");
-
-const boxGame = document.querySelector("#boxgame");
-
-const error = document.querySelector(".error");
-
-const arr = [red, blue, green, yellow];
-
-const start = document.querySelector(".start");
-
-const scr = document.querySelector(".scr span");
-const highestScr = document.querySelector(".highestScr span");
-
-let colorQueryArr = [];
-
-let score = 0;
-let highestScore = 0;
-
-// let flag;
-
-start.addEventListener("click", async (e) => {
-  e.preventDefault();
-  score = 0;
-  colorQueryArr = [];
-  if (!error.classList.contains("hidden")) {
-      error.classList.add("hidden");
-  }
-  start.classList.add("hidden");
-  await wait(400);
-  await startGame();
-});
-
-async function startGame() {
-
-
-
-    let rndmCol = randomColor();
-    console.log(rndmCol);
-    colorQueryArr.push(rndmCol);
-    for (const colDiv of colorQueryArr) {
-        await animation1(colDiv);
-    }
-
-    let clickCounter = 0
-
-    let hoverHandler = async(e) => {
-      e.preventDefault();
-      if (e.target == red || e.target == blue || e.target == green || e.target == yellow) {
-        e.target.style.backgroundColor = 'rgb(218, 215, 215)';
-      }
-    }
-
-    let mouseOutHandler = (e) => {
-      e.preventDefault();
-      if (e.target == red) {
-        e.target.style.backgroundColor = 'rgb(255, 0, 0)';
-      }
-      else if (e.target == blue) {
-        e.target.style.backgroundColor = 'rgb(0, 0, 255)';
-      }
-      else if (e.target == green) {
-        e.target.style.backgroundColor = 'rgb(0, 128, 0)';
-      }
-      else if (e.target == yellow) {
-        e.target.style.backgroundColor = 'rgb(255, 255, 0)';
-      }
-    };
-
-    // boxGame.removeEventListener("mouseover", hoverHandler)
-    // boxGame.removeEventListener('mouseout', mouseOutHandler);
-    // boxGame.addEventListener("mouseover", hoverHandler)
-    // boxGame.addEventListener('mouseout', mouseOutHandler);
-
-    let clickHandler = async (e) => {
-      e.preventDefault();
-      if (e.target == red || e.target == blue || e.target == green || e.target == yellow) {
-          // console.log("e.target.textContent", e.target.textContent)
-          // console.log("colorQueryArr[clickCounter - 1].textContent", colorQueryArr[clickCounter-1]["textContent"])
-          if (clickCounter < colorQueryArr.length) {
-            if (e.target.textContent === colorQueryArr[clickCounter].textContent) {
-                clickCounter++;
-                if (e.target.textContent == "red") {
-                  audio = new Audio(`./audios/correct/${1}.mp3`);
-                } else if (e.target.textContent == "grn") {
-                  audio = new Audio(`./audios/correct/${2}.mp3`);
-                } else if (e.target.textContent == "blue") {
-                  audio = new Audio(`./audios/correct/${3}.mp3`);
-                } else if (e.target.textContent == "yllo") {
-                  audio = new Audio(`./audios/correct/${4}.mp3`);
-                }
-                audio.play()
-                // let rndmNmbr = getRandomNumber(1, 4);
-                // console.log(rndmNmbr)
-                // let audio = new Audio(`./audios/correct/${rndmNmbr}.mp3`);
-                // audio.play();
-                await applyFilterWithAnimation(e.target, 'correct');
-                // console.log("nice")
-                // console.log("clickCounter", clickCounter)
-                console.log("colorQueryArr.length", colorQueryArr.length)
-                if (clickCounter == colorQueryArr.length) {
-                  boxGame.removeEventListener("click", clickHandler);
-                  // boxGame.removeEventListener("mouseover", hoverHandler)
-                  score++
-                  highestScore = Math.max(score, highestScore);
-                  scr.textContent = score;
-                  highestScr.textContent = highestScore;
-                  await wait(1000)
-                  await startGame();
-                }  
-            } 
-            else {
-                let audio = new Audio(`./audios/wrong/1.mp3`);
-                audio.play();
-                score = 0;
-                scr.textContent = score;
-                await applyFilterWithAnimation(e.target, 'wrong');
-                await errorAnimation();
-                colorQueryArr = [];
-                start.classList.remove("hidden");
-
-                // boxGame.removeEventListener('mouseout', mouseOutHandler);
-                boxGame.removeEventListener("click", clickHandler);
-            }
-          } 
-
-        }
-
-    }
-    boxGame.removeEventListener("click", clickHandler)
-    boxGame.addEventListener("click", clickHandler)
+:root {
+  --box-height: 11rem;
+  --box-width: 11rem;
+}
+  
+*, *::after, *::before {
+  margin: 0;
+  padding: 0;
 }
 
-function randomColor() {
-  return arr[Math.floor(Math.random() * 4)];
+body {
+  font-family: 'Roboto', sans-serif;
+  background-color: rgb(32, 32, 32);
+  min-width: fit-content;
 }
 
-function animation(x) {
-  return new Promise((resolve, reject) => {
-    let audio 
-    if (x.textContent == "red") {
-      audio = new Audio(`./audios/correct/${1}.mp3`);
-    } else if (x.textContent == "grn") {
-      audio = new Audio(`./audios/correct/${2}.mp3`);
-    } else if (x.textContent == "blue") {
-      audio = new Audio(`./audios/correct/${3}.mp3`);
-    } else if (x.textContent == "yllo") {
-      audio = new Audio(`./audios/correct/${4}.mp3`);
-    }
-    audio.play()
-    x.style.transition = "0.1s all ease-out";
-    x.style.transform = "scale(1.1)";
-    setTimeout(() => {
-      resolve();
-    }, 800);
-  });
+.heading {
+  display: block;
+  text-align: center;
+  background-color: rgb(0, 0, 0);
+  padding: 10px 0px;
+  font-size: xxx-large;
 }
 
-function animation2(x) {
-  return new Promise((resolve, reject) => {
-    x.style.transition = "0.3s all ease-in";
-    x.style.transform = "scale(1)";
-    setTimeout(() => {
-      resolve();
-    }, 300);
-  });
+.heading p {
+  color: white;
+  text-shadow: 2px 2px 2px rgb(105, 105, 105);
+}  
+
+.red, .blue, .yellow, .green {
+  height: var(--box-height);
+  width: var(--box-width);
+/*   background-color: red; */
+  /* border: 4px solid rgb(0, 0, 0); */
+  margin: 0.8rem;
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0px 0px 40px rgb(105, 105, 105);
 }
 
-async function animation1(x) {
-  await animation(x);
-  await animation2(x);
+/* .red:hover, .blue:hover, .yellow:hover, .green:hover {
+  background-color: rgb(218, 215, 215);
+} */
+
+.red {
+  background-color: red;
+  color: red;
+  box-shadow: 0px 0px 10px red;
 }
 
-
-async function errorAnimation() {
-  error.classList.remove("hidden");
-};
-
-async function wait(ms) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, ms)
-  })
+.blue {
+  background-color: blue;
+  color: blue;
+  box-shadow: 0px 0px 10px blue;
 }
 
-async function applyFilterWithAnimation(box, filterClass) {
-  box.classList.add(filterClass);
-  // box.style.transform = 'scale(1.1)';
-
-  setTimeout(() => {
-      box.style.transform = 'scale(1)';
-      box.classList.remove(filterClass);
-  }, 400); 
+.yellow {
+  background-color: yellow;
+  color: yellow;
+  box-shadow: 0px 0px 10px yellow;
 }
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+.green {
+  background-color: green;
+  color: green;
+  box-shadow: 0px 0px 10px green;
 }
 
+.red span, .blue span, .yellow span, .green span {
+  display: none;
+}
+
+.row1, .row2 {
+  display: flex;
+}
+
+.start {
+  padding: 0.3rem 1.8rem;
+  margin: 0.7rem 0 0 0;
+  cursor: pointer;
+  /* border: 4px solid rgb(255, 255, 255); */
+  border-radius: 10px;
+  max-width: fit-content;
+  box-shadow: 0px 0px 20px rgb(105, 105, 105);
+  font-size: x-large;
+}
+
+.start:hover {
+  background-color: rgb(0, 116, 124);
+  color: rgb(255, 255, 255);
+}
+
+.main-container{
+  display:flex;
+  flex-direction: column;
+  justify-content:center;
+  /* align-items:center; */
+  /* min-height: calc(100vh - 100px);
+  min-width: 100vw; */
+  padding: 1.4rem 0 0 0;
+}
+
+.click-animation {
+  scale: 1.1;
+  transition: 0.1s all ease-out;
+}
+
+.hidden {
+  display: none;
+}
+
+.error {
+  color: white;
+  /* text-shadow: 2px 2px 2px rgb(105, 105, 105); */
+  margin: 12px 0px 0px 0px;
+  background-color: rgb(160, 0, 0);
+  padding: 0.4rem;
+  border-radius: 10px;
+  /* border: 3px solid black; */
+  text-align: center;
+  max-width: fit-content;
+  box-shadow: 0px 0px 20px rgb(160, 0, 0);
+  font-size: x-large;;
+}
+
+.correct {
+  /* filter: hue-rotate(90deg) saturate(1.5) brightness(1.2); */
+  background-color: rgb(153, 255, 0)
+}
+
+.wrong {
+  /* filter: hue-rotate(-30deg) saturate(1.5) brightness(1.2); */
+  background-color: rgb(255, 108, 108);
+}
+
+#score {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+  text-shadow: 2px 2px 2px rgb(105, 105, 105);
+  margin: 0.5rem 0 0 0;
+  font-size: medium;
+}
+
+#score h2 span {
+  display: inline-block;
+  color: rgb(255, 216, 90);
+  margin: 5px;
+}
+
+#boxgame {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1.5rem 0 0 0;
+}
+
+#errorDiv {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#startDiv {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+  
